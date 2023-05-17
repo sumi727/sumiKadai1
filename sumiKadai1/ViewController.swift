5dc693e1cb3c5d4f57ff3fb28edc5010a60dfddc
sumi727 <youtaijiao530@gmail.com>
2023-05-17 15:33:46 +0900

    Initial Commit

---
+++ sumiKadai1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sumiKadai1
+//
+//  Created by 角友汰 on 2023/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
